1faa02055adc43fb300de24d96a4224de4e565cc
eric-do <ericdo.617@gmail.com>
2019-04-08 12:37:45 -0700

    First update

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,60 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+// Receives JS object
+// Need to convert to String
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Identify object type
+  // If array
+  // Add string with '[' + val + ']'
+  // Recursively call stringifyJSON
+  // If object
+  // Add string with '{' + val + '}'
+  // Recursively call stringifyJSON
+  // Else
+  // Return value.toString()
+  var str = '';
+  if (obj === null) {
+    return 'null';
+  }
+  
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    
+    for (var i = 0; i < obj.length; i++) {
+      if (Array.isArray(obj[i])) {
+        str += '[' + stringifyJSON(obj[i]) + ']';
+      } else if (typeof obj[i] === 'object') {
+        str += '{' + stringifyJSON(obj[i]) + '}';
+      } else {
+        str +=  '[' + stringifyJSON(obj[i]) + ']';
+      }
+    };
+  } else if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (Array.isArray(val)) {
+        str += '[' + stringifyJSON(val) + ']';
+      } else if (typeof val === 'object') {
+        str += '{' + stringifyJSON(val) + '}';
+      }
+    }
+  } else {
+    return obj.toString();
+  }
 };
+
+// function getString(val) {
+//   if (Array.isArray(val)) {
+//     str += '[' + stringifyJSON(val) + ']';
+//   } else if (typeof val === 'object') {
+//     str += '{' + stringifyJSON(val) + ']';
+//   }
+      
+// }
+
